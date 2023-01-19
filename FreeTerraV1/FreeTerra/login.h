d33e3962367b1b241dec6898104cae9cee8a70c0
Weedmanu <terraspi13@gmail.com>
2023-01-19 19:52:03 +0100

    Delete login.h

--- FreeTerraV1/FreeTerra/login.h
+++
@@ -1,8 +0,0 @@
-#define Wifi_H
-#ifdef Wifi_H
-
-#define RESEAU 	    "freeman"                // Changez votre reseau Wifi
-#define PASSWORD 	  "manu2612@SOSSO1008"     // Changez votre mot de passe reseau Wifi
-#define HOST 		    "FreeTerra"              // le nom de votre projet
-  
-#endif
